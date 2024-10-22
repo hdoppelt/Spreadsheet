4447c0624a1bc0d3c3d9645c50c652b919fff1a8
hdoppelt <u0674744@umail.utah.edu>
2024-10-22 16:07:21 -0600

    Added comments throughout files for better readability

--- Spreadsheet/GUI/Components/Pages/SpreadsheetPage.razor.cs
+++ Spreadsheet/GUI/Components/Pages/SpreadsheetPage.razor.cs
@@ -1,6 +1,9 @@
-﻿// <copyright file="SpreadsheetPage.razor.cs" company="UofU-CS3500">
-// Copyright (c) 2024 UofU-CS3500. All rights reserved.
-// </copyright>
+﻿/// <copyright file="SpreadsheetPage.razor.cs" company="UofU-CS3500">
+///     Copyright (c) 2024 UofU-CS3500. All rights reserved.
+/// </copyright>
+/// 
+/// Name: Harrison Doppelt & Victor Valdez Landa
+/// Date: 10/22/2024
 
 namespace GUI.Client.Pages;
 
@@ -12,56 +15,75 @@ using System.Diagnostics;
 using CS3500.Spreadsheet;
 
 /// <summary>
-/// TODO: Fill in
+///     TODO: Fill in
 /// </summary>
 public partial class SpreadsheetPage
 {
     /// <summary>
-    /// Based on your computer, you could shrink/grow this value based on performance.
+    ///     Based on your computer, you could shrink/grow this value based on performance.
     /// </summary>
     private const int ROWS = 50;
 
     /// <summary>
-    /// Number of columns, which will be labeled A-Z.
+    ///     Number of columns, which will be labeled A-Z.
     /// </summary>
     private const int COLS = 26;
 
     /// <summary>
-    /// Provides an easy way to convert from an index to a letter (0 -> A)
+    ///     Provides an easy way to convert from an index to a letter (0 -> A)
     /// </summary>
     private char[] Alphabet { get; } = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
 
-
     /// <summary>
-    ///   Gets or sets the name of the file to be saved
+    ///     Gets or sets the name of the file to be saved
     /// </summary>
     private string FileSaveName { get; set; } = "Spreadsheet.sprd";
 
-
     /// <summary>
-    ///   <para> Gets or sets the data for all of the cells in the spreadsheet GUI. </para>
-    ///   <remarks>Backing Store for HTML</remarks>
+    ///     <para> Gets or sets the data for all of the cells in the spreadsheet GUI. </para>
+    ///     <remarks>Backing Store for HTML</remarks>
     /// </summary>
     private string[,] CellsBackingStore { get; set; } = new string[ROWS, COLS];
 
-
-
-    //ADDED FROM LECTURE
-    // Member variables used to keep track of internal state, which as
-    // which row/col is selected, and to control UI elements
-    // See lecture for details
-    private string SelectedCell = "A1";
-    private string CurrentContents = string.Empty;
-    private ElementReference TextArea;
-    private int SelectedRow = 0;
-    private int SelectedCol = 0;
-    //Temporary
-    private Spreadsheet ss = new();
-    private object SelectedValue = "";
-
-    //ADDED FROM LECTURE
     /// <summary>
-    /// Handler for when a cell is clicked
+    ///     Default starting cell name.
+    /// </summary>
+    private string SelectedCell = "A1";
+
+    /// <summary>
+    ///     Default empty cell content.
+    /// </summary>
+    private string CurrentContents = string.Empty;
+    private string SelectedContents = string.Empty;
+
+    /// <summary>
+    ///     Default empty cell value.
+    /// </summary>
+    private object SelectedValue = string.Empty;
+
+    /// <summary>
+    ///     The currently selected row in the spreadsheet. Defaults to row 0 (first row).
+    ///     This is used to keep track of which row is active based on user interaction.
+    /// </summary>
+    private int SelectedRow = 0;
+
+    /// <summary>
+    ///     The currently selected column in the spreadsheet. Defaults to column 0 (first column).
+    ///     This is used to keep track of which column is active based on user interaction.
+    /// </summary>
+    private int SelectedCol = 0;
+
+    /// <summary>
+    ///     Reference to the HTML TextArea element used in the spreadsheet for editing cell contents.
+    ///     This is used to programmatically focus on or manipulate the text area.
+    /// </summary>
+    private ElementReference TextArea;
+
+    // Initialize new spreadsheet object
+    private Spreadsheet sheet = new();
+
+    /// <summary>
+    ///     Handler for when a cell is clicked
     /// </summary>
     /// <param name="row">The row component of the cell's coordinates</param>
     /// <param name="col">The column component of the cell's coordinates</param>
@@ -70,12 +92,14 @@ public partial class SpreadsheetPage
         SelectedCell = $"{Alphabet[col]}{row + 1}";
         SelectedRow = row;
         SelectedCol = col;
-        CurrentContents = CellsBackingStore[row, col];
-        SelectedValue = ss.GetCellValue(SelectedCell);
+        SelectedContents = CellsBackingStore[row, col];
+        SelectedValue = sheet.GetCellValue(SelectedCell);
         TextArea.FocusAsync();
     }
 
-
+    /// <summary>
+    ///     TODO: Fill in
+    /// </summary>
     private async Task CellContentChanged(ChangeEventArgs e)
     {
         string data = e.Value!.ToString() ?? "";
@@ -83,14 +107,14 @@ public partial class SpreadsheetPage
         try
         {
             // Attempt to set the content in the spreadsheet
-            ss.SetContentsOfCell(SelectedCell, data);
+            sheet.SetContentsOfCell(SelectedCell, data);
 
             // Recalculate the value of the selected cell
-            SelectedValue = ss.GetCellValue(SelectedCell);
+            SelectedValue = sheet.GetCellValue(SelectedCell);
 
             // Update the current contents and backing store
             CellsBackingStore[SelectedRow, SelectedCol] = data;
-            CurrentContents = data;
+            SelectedContents = data;
 
             // Refocus the text area
             await TextArea.FocusAsync();
@@ -102,13 +126,9 @@ public partial class SpreadsheetPage
         }
     }
 
-
-
-
-
     /// <summary>
-    /// Saves the current spreadsheet, by providing a download of a file
-    /// containing the json representation of the spreadsheet.
+    ///     Saves the current spreadsheet, by providing a download of a file
+    ///     containing the json representation of the spreadsheet.
     /// </summary>
     private async void SaveFile()
     {
@@ -117,12 +137,12 @@ public partial class SpreadsheetPage
     }
 
     /// <summary>
-    /// This method will run when the file chooser is used, for loading a file.
-    /// Uploads a file containing a json representation of a spreadsheet, and 
-    /// replaces the current sheet with the loaded one.
+    ///     This method will run when the file chooser is used, for loading a file.
+    ///     Uploads a file containing a json representation of a spreadsheet, and 
+    ///     replaces the current sheet with the loaded one.
     /// </summary>
     /// <param name="args">The event arguments, which contains the selected file name</param>
-    private async void HandleFileChooser( EventArgs args )
+    private async void HandleFileChooser(EventArgs args)
     {
         try
         {
